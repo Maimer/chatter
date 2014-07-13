eaed773a89a5345b155bfede41a1ece29d5a69bb
Maimer <nesnl23@yahoo.com>
2014-07-13 13:45:59 -0400

    Added validation to user model so that handles are not case sensitive (ie can't register Nick and nIck)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :rememberable,
+         :trackable,
+         :validatable
 
-  validates :handle, presence: true, uniqueness: true,
-            format: { with: /\A[\w]+\z/ }, length: { minimum: 4, maximum: 12 },
+  validates :handle, presence: true,
+            format: { with: /\A[\w]+\z/ },
+            length: { minimum: 4, maximum: 12 },
             exclusion: { in: %w(Admin Server) }
+
+  validates_uniqueness_of :handle, case_sensitive: false
 end
